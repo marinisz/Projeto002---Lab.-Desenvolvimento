a87db662171b3285ab849a6708434b7ce370fe93
Vinícius Marini <marini_costa@hotmail.com>
2021-09-26 09:40:27 -0300

    apagando pasta crudrent

--- Código/Sistema_de_Aluguel_de_Carros/src/main/java/com/example/sistema_de_aluguel_de_carros/Models/Agente.java
+++
@@ -1,7 +0,0 @@
-package com.example.sistema_de_aluguel_de_carros.Models;
-
-public class Agente extends Usuario {
-
-	private String CNPJ;
-
-}
--- Código/Sistema_de_Aluguel_de_Carros/src/main/java/com/example/sistema_de_aluguel_de_carros/Models/Rendimento.java
+++
@@ -1,9 +0,0 @@
-package com.example.sistema_de_aluguel_de_carros.Models;
-
-public class Rendimento {
-
-	private String nome;
-
-	private double valor;
-
-}
