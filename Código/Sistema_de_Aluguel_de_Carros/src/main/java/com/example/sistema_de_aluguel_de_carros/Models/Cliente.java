aec6d1ed18d4f1c04f51cbd3523d068ae4a2be12
Vinícius Marini <marini_costa@hotmail.com>
2021-09-26 09:58:33 -0300

    vinicius

---
+++ Código/Sistema_de_Aluguel_de_Carros/src/main/java/com/example/sistema_de_aluguel_de_carros/Models/Cliente.java
@@ -0,0 +1,15 @@
+package com.example.sistema_de_aluguel_de_carros.Models;
+
+import java.util.List;
+
+public class Cliente extends Usuario {
+
+	private String RG;
+
+	private String CPF;
+
+	private String profissao;
+
+	private List<Rendimento> rendimentos;
+
+}
---
+++ Código/Sistema_de_Aluguel_de_Carros/src/main/java/com/example/sistema_de_aluguel_de_carros/Models/Veiculo.java
@@ -0,0 +1,15 @@
+package com.example.sistema_de_aluguel_de_carros.Models;
+
+public class Veiculo {
+
+	private String matricula;
+
+	private int ano;
+
+	private String marca;
+
+	private String modelo;
+
+	private String placa;
+
+}
